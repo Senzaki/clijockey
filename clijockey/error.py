3cdb339b1851aaa49d5ab8ba0cf863b2ac8ccd33
mpenning <mike@pennington.net>
2016-12-15 14:18:01 -0600

    Add more errors

--- clijockey/error.py
+++ clijockey/error.py
@@ -5,3 +5,11 @@ class ConnectionFailedError(Exception):
 class AuthenticationFailedError(Exception):
     def __init__(self, *args, **kwargs):
         super(AuthenticationFailedError, self).__init__(*args, **kwargs)
+
+class ResponseFailException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(ResponseFailException, self).__init__(*args, **kwargs)
+
+class UnexpectedConnectionClose(Exception):
+    def __init__(self, *args, **kwargs):
+        super(UnexpectedConnectionClose, self).__init__(*args, **kwargs)
