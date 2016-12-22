5998d1bcc7a4c0b68abf19db78fd154510c4ac95
mpenning <mike@pennington.net>
2016-12-22 15:44:49 -0600

    Add TraitTable()

--- clijockey/util.py
+++ clijockey/util.py
@@ -1,10 +1,33 @@
 from logging.handlers import TimedRotatingFileHandler
 from logging.handlers import MemoryHandler
+from abc import abstractmethod
 import logging
 import atexit
 import sys
 
+from traitlets import CUnicode, CInt, CFloat, CLong, CBytes, CBool
+from traitlets import Unicode, Int, Float, Long, Bytes, Bool
+from traitlets import CaselessStrEnum, Enum, UseEnum
+from traitlets import CRegExp
+from traitlets import HasTraits, Union, validate
+from traitlets import Dict, Tuple, Set, List
 import arrow
+
+#
+#   "David Michael Pennington" <mike@pennington.net>
+#   Copyright 2016
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 class Account(object):
     def __init__(self, username="", password="", priv_password=""):
@@ -72,6 +95,21 @@ class RotatingTOMLLog(object):
         )
         atexit.register(self.flush)
 
+    def _toml_value(self, val):
+        """Convert a python variable to a valid TOML value (as a string)"""
+        if isinstance(val, str) or isinstance(val, unicode):
+            # Properly quote strings and unicode
+            return '"{0}"'.format(val)
+        elif getattr(val, 'isoformat', None):
+            # Handle an arrow object...
+            return '{0}'.format(val.isoformat())
+        elif isinstance(val, bool) and (val is True):
+            return 'true'
+        elif isinstance(val, bool) and (val is False):
+            return 'false'
+        else:
+            return '{0}'.format(val)
+
     def write_table(self, table="", info={}, timestamp=False):
         assert table!=""
         assert isinstance(info, dict)
@@ -86,18 +124,12 @@ class RotatingTOMLLog(object):
 
         self.log.debug('[{0}]'.format(table))
         for key, val in info.items():
-            if isinstance(val, str):
-                self.log.debug('{0} = "{1}"'.format(key, val))
-            elif getattr(val, 'isoformat', None):
-                self.log.debug('{0} = {1}'.format(key, val.isoformat()))
-            else:
-                self.log.debug('{0} = {1}'.format(key, val))
+            self.log.debug('{0} = {1}'.format(key, self._toml_value(val)))
 
     def write_table_list(self, table="", info={}, timestamp=False):
         assert table!=""
         assert isinstance(info, dict)
         assert info!={}
-
 
         if timestamp and isinstance(timestamp, bool):
             now = arrow.now()
@@ -108,12 +140,7 @@ class RotatingTOMLLog(object):
 
         self.log.debug('[[{0}]]'.format(table))
         for key, val in info.items():
-            if isinstance(val, str):
-                self.log.debug('{0} = "{1}"'.format(key, val))
-            elif getattr(val, 'isoformat', None):
-                self.log.debug('{0} = {1}'.format(key, val.isoformat()))
-            else:
-                self.log.debug('{0} = {1}'.format(key, val))
+            self.log.debug('{0} = {1}'.format(key, self._toml_value(val)))
 
     def write(self, msg):
         self.log.debug(msg)
@@ -121,3 +148,25 @@ class RotatingTOMLLog(object):
     def flush(self):
         self.memoryhandler.flush()
 
+class TraitTable(HasTraits):
+    _map = Tuple()    # Key-Value map
+    def __init__(self, *args, **kwargs):
+        #map = kwargs.get('map', None) or args[0]
+        values = kwargs.get('values', None) or args[0]
+        #assert map is not None
+        #assert isinstance(map, tuple) or isinstance(map, list)
+        assert values is not None
+        assert isinstance(values, tuple) or isinstance(values, list)
+
+        self._dict = dict()
+        for attr_name, value in zip(self._map, values):
+            setattr(self, attr_name, value)  # Set the value, through traitlets
+            self._dict[attr_name] = getattr(self, attr_name) # Get the value
+
+    def as_dict(self):
+        return self._dict
+
+    # http://stackoverflow.com/a/35282286
+    def __iter__(self):
+        for attr_name, val in self._dict.items():
+            yield attr_name, val
